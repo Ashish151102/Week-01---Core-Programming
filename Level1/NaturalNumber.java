9b134270939abaf7ff8b6bcbcdacab74fe8ed70c
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-10 23:07:59 +0530

    calculate natural number

---
+++ Level1/NaturalNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class NaturalNumber{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+		// Taking input 
+		System.out.print("Enter the number - ");
+		int inputNumber = input.nextInt();
+		
+        // Check if the number is a natural number
+        if (inputNumber > 0) {
+		
+            // Calculate the sum of the first n natural numbers
+            int sumOfNaturalNumbers = inputNumber * (inputNumber + 1) / 2;
+            System.out.println("The sum of the first " + inputNumber + " natural numbers is " + sumOfNaturalNumbers);
+        } else {
+            System.out.println("The number is not a natural number");
+        }
+		
+		input.close();
+    }
+}
