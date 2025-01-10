1a36d82677c2a442714d216a841028bcde33ce79
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-10 23:03:54 +0530

    calculating factorial using while loop

---
+++ Level1/FactorialWhileLoop.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class FactorialWhileLoop{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        // Check if the input is a positive integer
+        if (number < 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            // Compute factorial using a while loop
+            int factorial = 1;
+            int currentMultiplier = 1;
+
+            while (currentMultiplier <= number) {
+                factorial *= currentMultiplier;
+                currentMultiplier++;
+            }
+
+            System.out.println("The factorial of " + number + " is: " + factorial);
+        }
+
+        input.close();
+    }
+}
