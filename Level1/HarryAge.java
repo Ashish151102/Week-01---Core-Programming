e3aee48108c6c4e251f68e4283259babb88d71d6
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-13 15:07:38 +0530

    program to find the age of Harry if the birth year is 2000

---
+++ Level1/HarryAge.java
@@ -0,0 +1,14 @@
+class HarryAge {
+    public static void main(String[] args) {
+	
+	//Create a int variable and assigned value
+	  int BirthYear = 2000;
+	  int CurrentYear = 2024;
+	  
+	  //Create a int variable Harry's Age and Calculate Harry's Age
+	  int Harrys_Age = CurrentYear - BirthYear;
+	  
+	  //Print Harry's age
+	  System.out.println("Harry's age in 2024 is " + Harrys_Age);
+    }
+}	
